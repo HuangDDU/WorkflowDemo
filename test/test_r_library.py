47955ffbc7f993a34c5093c8a231859a49770e3e
黄照样 <554598474@qq.com>
2024-12-19 17:13:22 +0800

    test_r_library

---
+++ test/test_r_library.py
@@ -0,0 +1,16 @@
+import pytest
+import rpy2.robjects as ro
+import rpy2.rinterface as rinterface
+
+
+def test_r_library():
+    # 测试rpy2, 调用R的library，查看库是否安装成功
+    r_script = """
+    library(dynwrap)
+    """
+    ro.r(r_script)
+
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", __file__])
