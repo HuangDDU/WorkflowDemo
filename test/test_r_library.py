54e52c4064baffc79b10703edfddd95a71e50736
黄照样 <554598474@qq.com>
2024-12-23 09:57:09 +0800

    文档构建

--- test/test_r_library.py
+++ test/test_r_library.py
@@ -1,15 +1,15 @@
-import pytest
-import rpy2.robjects as ro
+# import pytest
+# import rpy2.robjects as ro
 
 
-@pytest.mark.skipif(True, reason="Skip R")
-def test_r_library():
-    # 测试rpy2, 调用R的library，查看库是否安装成功
-    r_script = """
-    library(dynwrap)
-    """
-    ro.r(r_script)
+# @pytest.mark.skipif(True, reason="Skip R")
+# def test_r_library():
+#     # 测试rpy2, 调用R的library，查看库是否安装成功
+#     r_script = """
+#     library(dynwrap)
+#     """
+#     ro.r(r_script)
 
 
-if __name__ == "__main__":
-    pytest.main(["-v", __file__])
+# if __name__ == "__main__":
+#     pytest.main(["-v", __file__])
--- test/test_rscript_add.py
+++ test/test_rscript_add.py
@@ -1,39 +1,39 @@
-import pytest
-import rpy2.robjects as ro
-import rpy2.rinterface as rinterface
+# import pytest
+# import rpy2.robjects as ro
+# import rpy2.rinterface as rinterface
 
 
-# 自动的数据转换
-@ro.conversion.rpy2py.register(rinterface.IntSexpVector)
-def convert_integer(obj):
-    if len(obj) == 1:
-        return int(obj[0])
-    else:
-        return [int(x) for x in obj]
+# # 自动的数据转换
+# @ro.conversion.rpy2py.register(rinterface.IntSexpVector)
+# def convert_integer(obj):
+#     if len(obj) == 1:
+#         return int(obj[0])
+#     else:
+#         return [int(x) for x in obj]
 
 
-@ro.conversion.rpy2py.register(rinterface.FloatSexpVector)
-def convert_double(obj):
-    if len(obj) == 1:
-        return float(obj[0])
-    else:
-        return [float(x) for x in obj]
+# @ro.conversion.rpy2py.register(rinterface.FloatSexpVector)
+# def convert_double(obj):
+#     if len(obj) == 1:
+#         return float(obj[0])
+#     else:
+#         return [float(x) for x in obj]
 
 
-@pytest.mark.skipif(True, reason="Skip R")
-def test_rscript_add():
-    # 测试rpy2, 调用R脚本
-    r_script = """
-    add <- function(a, b) {
-        return (a+b)
-    }
-    c <- add(1,2)
-    c
-    """
-    c = ro.r(r_script)
-    # 获得输出并判断
-    assert c == 3
+# @pytest.mark.skipif(True, reason="Skip R")
+# def test_rscript_add():
+#     # 测试rpy2, 调用R脚本
+#     r_script = """
+#     add <- function(a, b) {
+#         return (a+b)
+#     }
+#     c <- add(1,2)
+#     c
+#     """
+#     c = ro.r(r_script)
+#     # 获得输出并判断
+#     assert c == 3
 
 
-if __name__ == "__main__":
-    pytest.main(["-v", __file__])
+# if __name__ == "__main__":
+#     pytest.main(["-v", __file__])
